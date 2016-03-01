9337f66850522d841073f791f8982a34a5b8a88b
Andrew Penkrat <contact.aldrog@gmail.com>
2016-03-01 16:56:48 +0300

    Calculate moments corresponding to p(x) = (b - x)^(-2/5).

--- task2-newton-cotes/function.nim
+++ task2-newton-cotes/function.nim
@@ -3,9 +3,19 @@ import math
 const
     a* = 2.3
     b* = 2.9
-    preciseResult* = 163.06157005314853
+    preciseResult* = 348.8181344253912
+    beta = 2/5
 
 proc f*(x: float): float {.procvar.} = 3*cos(2.5*x)*exp(7*x/4) + 5*sin(0.5*x)*exp(3*x/8) + 4
 
-proc moment*(i: int): float {.procvar.} = (1 / (i+1)) * (pow(b, i.float + 1) - pow(a, i.float + 1))
+proc moment*(k: int): float {.procvar.} =
+    # Integral [(b-x)^k / x^(2/5), {x, 0, b-a}]
+    for i in 0..k:
+        let power = i.float - beta
+        var sumPart = (if i mod 2 == 0: 1.0 else: -1.0)
+        sumPart /= power + 1
+        sumPart *= binom(k, i).float
+        sumPart *= pow(b, (k-i).float)
+        sumPart *= pow(b - a, power + 1)
+        result += sumPart
 
